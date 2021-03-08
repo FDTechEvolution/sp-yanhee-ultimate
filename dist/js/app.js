a7df543436b34947216cdcee2611bf4b551c42dd
artdata1 <48270726+artdata1@users.noreply.github.com>
2021-03-08 12:41:36 +0700

    editNewForm

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,130 +1,38 @@
-Vue.component('button-order', {
-    data: function () {
-      return {}
-    },
-    template: `<a href="#" class="btn btn-primary btn-lg rounded text-center w-100"><i class="bi bi-cart4"></i> สั่งซื้อ</a>`
-})
-  
-Vue.component('button-contact', {
-    data: function () {
-      return {}
-    },
-    template: `<a href="#" class=""><img src="https://sp-dist.orderpang-sv.com/dist/img/btn_line_contact.png" class="w-100"/></a>`
-})
-  
-Vue.component('button-full', {
-    data: function () {
-      return {}
-    },
-    template: `<div class="row my-5">
-                <div class="col-lg-5 col-md-6 d-md-grid gap-2 mx-auto col-sm-12 text-center">
-                  <button-order></button-order>
-                </div>
-                <div class="col-lg-2 col-md-12 text-center my-md-3">-หรือ-</div>
-                <div class="col-lg-5 col-md-6 d-md-grid gap-2 mx-auto col-sm-12 text-center">
-                  <button-contact></button-contact>
-                </div>
-              </div>`
-})
-  
-Vue.component("modal", {
-    data() {
-      return {
-        isProduct: 'Yanhee Ultimate',
-        products: [
-          {name: 'ชุดกู้ระบบเผาผลาญ 1 แถม 1', price: 1200, discount: 990, src: 'dist/img/promotion/Ultimate_210219_28.jpg'},
-          {name: 'ชุดโปรเร่งเบิร์น 2 แถม 2', price: 2400, discount: 1890, src: 'dist/img/promotion/Ultimate_210219_29.jpg'}
-        ],
-        productSelect: '',
-        customerData: {
-            name: '',
-            address: '',
-            mobile: ''
-        }
-      }
-    },
-    computed: {
-        confirmOrder() {
-            return (this.productSelect !== '' && this.customerData.name !== '' && this.customerData.address !== '' && this.customerData.mobile !== '') ? true : false
-        }
-    },
-    methods: {
+Vue.component("thank-you", {
+  template: `<transition name="modal">
+              <div class="modal-mask">
+                <div class="modal-wrapper">
+                  <div class="modal-container">
 
-    },
-    template: `<transition name="modal">
-                <div class="modal-mask">
-                  <div class="modal-wrapper">
-                    <div class="modal-container">
-  
-                      <div class="modal-header">
-                        <div class="start-0">
-                            <slot name="header"><h4>ยืนยันการสั่งซื้อ</h4></slot>
-                        </div>
-                        <div class="end-0">
-                            <button class="btn btn-secondary" @click="$emit('close')">X</button>
-                        </div>
-                      </div>
-  
-                      <div class="modal-body">
-                        <slot name="body">
-                          <div class="row">
-                            <div class="col-lg-5 col-sm-12 text-center">
-                              <img v-if="productSelect" :src="productSelect.src" class="img-fluid">
-                              <img v-else src="dist/img/yh_ultm_product.png" class="img-fluid">
-                            </div>
-                            <div class="col-lg-7 col-sm-12 form-check">
-                              <div v-for="(product, index) in products">
-                                <input v-model="productSelect" type="radio" class="form-check-input" name="product" :id="index" :value="product" checked> <label :for="index">{{product.name}}</label>
-                              </div>
-                              <hr />
-                              <div class="card">
-                                <div v-if="productSelect" class="card-body text-end expandable">
-                                    <p><strong>ค่าสินค้า :</strong> {{productSelect.price}}</p>
-                                    <p><strong>ส่วนลด :</strong> -{{productSelect.price - productSelect.discount}}</p>
-                                    <p><strong>ราคาสุทธิ :</strong> {{productSelect.discount}}</p>
-                                </div>
-                                <div v-else class="card-body text-center">กรุณาเลือกรายการสินค้า</div>
-                              </div>
-                            </div>
-                          </div>
-                          <hr />
-                          <div class="row">
-                            <div class="col-sm-12">
-                                <div class="mb-3">
-                                    <label for="name" class="form-label">ชื่อ - สกุล</label>
-                                    <input v-model="customerData.name" type="text" class="form-control" id="name" placeholder="ชื่อผู้รับ">
-                                </div>
-                                <div class="mb-3">
-                                    <label for="address" class="form-label">ที่อยู่</label>
-                                    <textarea v-model="customerData.address" class="form-control" id="address" rows="3" placeholder="ที่อยู่สำหรับรับสินค้า"></textarea>
-                                </div>
-                                <div class="mb-3">
-                                    <label for="mobile" class="form-label">เบอร์โทร</label>
-                                    <input v-model="customerData.mobile" type="number" class="form-control" id="mobile" placeholder="โทรศัพท์มือถือ">
-                                </div>
-                            </div>
-                          </div>
-                        </slot>
-                      </div>
-  
-                      <div class="modal-footer">
-                        <slot name="footer">
-                          <button v-if="confirmOrder" class="btn btn-success" @click="$emit('close')">ยืนยัน</button>
-                          <button v-else class="btn btn-success" disabled>ยืนยัน</button>
-                          <button class="btn btn-secondary" @click="$emit('close')">ยกเลิก</button>
-                        </slot>
-                      </div>
+                    <div class="modal-body my-0 text-center">
+                      <slot v-if="$parent.responseStatus === 200" name="body">
+                        <h4 class="text-success">ขอบคุณสำหรับการสั่งซื้อ</h4>
+                        <p>ทางเราจะทำการจัดส่งสินค้าให้ท่านทันที</p>
+                      </slot>
+                      
+                      <slot v-else name="body">
+                        <h3 class="text-danger">เกิดข้อผิดพลาด</h3>
+                        <p>กรุณาลองใหม่อีกครั้ง หรือ สั่งซื้อผ่านทางไลน์</p>
+                      </slot>
                     </div>
+
+                    <div class="modal-footer mb-0 py-0 end-0">
+                      <button class="btn btn-secondary btn-sm" @click="$parent.showResponse = false">X ปิด</button>
+                    </div>
+
                   </div>
                 </div>
-              </transition>`
-});
+              </div>
+            </transition>`
+})
   
 new Vue({
     el: '#app',
     data() {
       return {
         showModal: false,
+        showResponse: false,
+        responseStatus: '',
         beforeAfter: [
           {src: 'dist/img/before_after/Ultimate_210219_17.jpg', alt: 'YANHEE ULTIMATE - Before & After', title: 'Before & After'},
           {src: 'dist/img/before_after/Ultimate_210219_18.jpg', alt: 'YANHEE ULTIMATE - Before & After', title: 'Before & After'},
@@ -141,7 +49,56 @@ new Vue({
           {src: 'dist/img/factory/C.2-1_V.1.jpg', alt: 'มาตรฐานการผลิตระดับสากล', title: ''},
           {src: 'dist/img/factory/C.1_V.3-2.jpg', alt: 'มาตรฐานการผลิตระดับสากล', title: ''},
           {src: 'dist/img/factory/C.1_V.4-1.jpg', alt: 'มาตรฐานการผลิตระดับสากล', title: ''}
-        ]
+        ],
+        formData: {
+          name: '',
+          address: '',
+          mobile: '',
+          product: '',
+          price: '',
+          source: '',
+          description: ''
+        }
+      }
+    },
+    computed: {
+      checkFormOrder() {
+        return (this.formData.name !== '' 
+                && this.formData.address !== '' 
+                && this.formData.mobile !== '' 
+                && this.formData.product !== '')
+                ? true : false
+      }
+    },
+    methods: {
+      confirmOrder() {
+        if(this.checkFormOrder) {
+          let price = this.formData.product.split(" ")
+          this.formData.product = price[0] + ' ' + price[1]
+          this.formData.price = price[2]
+          this.formData.source = document.location.origin
+          try{
+            axios.post('https://shop.orderpang-sv.com/tmporder/save', this.formData , {
+              headers: {
+                'Content-Type' : 'application/json'
+              }
+            })
+            .then((response) => {
+              console.log(response)
+              this.showResponse = true
+              this.responseStatus = response.status
+            })
+            .finally(() => {
+              this.formData.name = ''
+              this.formData.address = ''
+              this.formData.mobile = ''
+              this.formData.product = ''
+              this.formData.description = ''
+            })
+          }catch(e) {
+            throw e
+          }
+        }
       }
     }
 })
--- index.php
+++ index.php
@@ -21,8 +21,9 @@
     <!--https://icons.getbootstrap.com/-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
 
-    <!-- Vue.js -->
+    <!-- Vue.js & Axios -->
     <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
   </head>
 
 <body>    
@@ -40,7 +41,7 @@
     <div class="row gutter-0">
    
       <div class="col-12 px-0 d-grid gap-2 mx-auto">
-        <button type="button" class="btn btn-success btn-lg rounded-0" id="show-modal" @click="showModal = true"><i class="bi bi-cart4"></i> สั่งซื้อ ราคาพิเศษ 10 ท่านแรก</button>
+        <a href="#box-form" class="btn btn-success btn-lg rounded-0"><i class="bi bi-cart4"></i> สั่งซื้อ ราคาพิเศษ 10 ท่านแรก</a>
       </div>
     </div>
   </div>
@@ -135,7 +136,9 @@
         <h2 class="mb-1">การันตีจากลูกค้า</h2>
         <span>ผลลัพท์ดี ใช้ดี จึงบอกต่อ</span>
       </div>
-     
+      <div v-for="BnA in beforeAfter" class="col-lg-6 col-sm-12 mt-3">
+        <img :src="BnA.src" :title="BnA.title" :alt="BnA.alt" class="img-fluid">
+      </div>
     </div>
 
     
@@ -145,41 +148,42 @@
         <h2>กรอกข้อมูลเพื่อสั่งซื้อ</h2>
       </div>
       <div class="col-12">
-        <form name="frm" id="frm" method="POST">
+        
           <div class="mb-3">
             <label for="name" class="form-label"><strong>สินค้า/โปรโมชั่นที่ต้องการ</strong></label>
             <div class="form-check">
-              <input class="form-check-input" type="radio" name="product" id="pro1" value="ยันฮี 1แถม1 1040" checked="checked">
+              <input v-model="formData.product" class="form-check-input" type="radio" name="product" id="pro1" value="ยันฮี 1แถม1 1040" checked="checked">
               <label class="form-check-label" for="pro1">1แถม1 <del>1,200</del> เพียง 990฿ <span class="text-danger blink_me">ค่าส่ง 50฿</span> รวม 1,040฿</label>
             </div>
             <div class="form-check">
-              <input class="form-check-input" type="radio" name="product" id="pro2" value="ยันฮี 2แถม2 1890">
+              <input v-model="formData.product" class="form-check-input" type="radio" name="product" id="pro2" value="ยันฮี 2แถม2 1890">
               <label class="form-check-label" for="pro2">2แถม2 <del>2,400</del> เพียง 1,890฿ <span class="text-success blink_me">ส่งฟรี!</span></label>
             </div>
           </div>
           <div class="mb-3">
             <label for="name" class="form-label">ชื่อของคุณ</label>
-            <input type="text" class="form-control" id="name" name="name" placeholder="" required="required">
+            <input v-model="formData.name" type="text" class="form-control" id="name" name="name" placeholder="" required="required">
           </div>
           <div class="mb-3">
             <label for="mobile" class="form-label">เบอร์มือถือของคุณ <span class="text-info"><small>เราจะติดต่อคุณด้วยเบอร์นี้</small></span></label>
-            <input type="tel" class="form-control" id="mobile" name="mobile" placeholder="" required="required">
+            <input v-model="formData.mobile" type="tel" class="form-control" id="mobile" name="mobile" placeholder="" required="required">
           </div>
           <div class="mb-3">
             <label for="address" class="form-label">ที่อยู่สำหรับจัดส่งสินค้า</label>
-            <textarea class="form-control" id="address" name="address" rows="3" required="required"></textarea>
+            <textarea v-model="formData.address" class="form-control" id="address" name="address" rows="3" required="required"></textarea>
           </div>
           <div class="mb-3">
             <label for="description" class="form-label">หมายเหตุ/ฝากข้อความ (ไม่ระบุก็ได้)</label>
-            <textarea class="form-control" id="description" name="description" rows="3"></textarea>
+            <textarea v-model="formData.description" class="form-control" id="description" name="description" rows="3"></textarea>
           </div>
           <div class="mb-3 text-center">
-            <button class="btn btn-success btn-lg">ยืนยันการสั่งซื้อ</button>
+            <button :disabled="!checkFormOrder" class="btn btn-success btn-lg" @click="confirmOrder()">ยืนยันการสั่งซื้อ</button>
           </div>
-        </form>
+        
       </div>
     </div>
 
+    <thank-you v-if="showResponse"></thank-you>
     
   </main>
 
@@ -190,3 +194,6 @@
   </footer>  
   </body>
 </html>
+
+
+<script src="dist/js/app.js"></script>
